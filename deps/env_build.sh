afe6fd38736885db1bfa77737dd4cfe896d23682
Andrey Tovchigrechko <atovtchi@jcvi.org>
2010-07-06 17:34:06 -0400

    Added gcc41 to environ and not to init python vars before python is available

--- deps/env_build.sh
+++ deps/env_build.sh
@@ -10,12 +10,17 @@ if [ -z "$AT_ENV_RUN_DONE" ]; then
     exit 1
 fi
 
+# If python has not been built yet, do not define related vars
+if [ -f "$PYTHON" ]; then
+
 #Some variables that help with Python extension module building for
 #those installers that do not use distutils directly
 export PY_INC_DIR=`$PYTHON -c 'from distutils.sysconfig import *; print get_python_inc()'`
 #get_config_var() is "build dependent"
 export PY_LIB_DIR=`$PYTHON -c 'from distutils.sysconfig import *; print get_config_var("LIBPL")'`
 export PY_LIB=`$PYTHON -c 'from distutils.sysconfig import *; print get_config_var("LIBRARY")'`
+
+fi #$PYTHON
 
 #For C and C++ builds
 export CFLAGS="-I${INSTMACH}/include -I${INST}/include ${CFLAGS}"
