0264953cc1a6f67ddc66c366bee28952ccaf4e2c
Andrey Tovchigrechko <atovtchi@jcvi.org>
2010-07-15 23:21:12 -0400

    Build fixes and a fix to import Debug from PyMol

--- MGT/Debug.py
+++ MGT/Debug.py
@@ -7,25 +7,39 @@
 
 
 """Some support for logging and debugging."""
-__all__ = ["pdb","set_trace","Memuse"]
+__all__ = ["pdb","set_trace","Memuse","debugMsg"]
 
-
-#This tries to use IPython shell support if available
+# This try block will ignore an error when importing
+# this module from PyMol, which has a 'cmd' module that
+# supecedes standard 'cmd' module and causes 'pdb'
+# import to fail. Pdb will not be available in that case.
 try:
-    from IPython.Debugger import Pdb
-    def set_trace():
+    #This tries to use IPython shell support if available
+    try:
         from IPython.Debugger import Pdb
-        Pdb().set_trace()
-    del Pdb
-except:
-    import pdb
-    def set_trace():
+        def set_trace():
+            from IPython.Debugger import Pdb
+            Pdb().set_trace()
+        del Pdb
+    except:
         import pdb
-        pdb.set_trace()
-    del pdb
+        def set_trace():
+            import pdb
+            pdb.set_trace()
+        del pdb
 
-import pdb #standard pdb.set_trace() use
+    import pdb #standard pdb.set_trace() use
+except AttributeError:
+    pdb = None
 
+def debugMsg(msg,level=1):
+    """
+    Conditionally print a message.
+    Prints msg if the current debug level in the 
+    global options instance is at least at level 
+    supplied to this function."""
+    if level <= options.debug:
+        print msg
 
 from Bits import Memuse
 
