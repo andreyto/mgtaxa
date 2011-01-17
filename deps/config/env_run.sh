a12b5ea74707865b1d12b89797f1b2af2e17455a
Andrey Tovchigrechko <atovtchi@jcvi.org>
2011-01-17 00:57:14 -0500

    Build and postinstall of PostgreSQL server for Galaxy

--- deps/config/env_run.sh
+++ deps/config/env_run.sh
@@ -26,6 +26,12 @@ export INST_LIB_MACH=$INSTMACH/lib64
 
 export INST_LIB=$INSTMACH/lib
 
+## The location of files with initialization data
+## that should remain private and not go into a public repo 
+## - such as initial PostgreSQL root password.
+## This directory should be readable by user only
+export PRIV_DATA=$HOME/.priv_data
+
 if [ -n "$PY_PREF_INTERP" ]; then
 ## For python under its own prefix, we need to add paths to relevant env vars
 export LD_LIBRARY_PATH="$PY_PREF_INTERP/lib:$LD_LIBRARY_PATH"
@@ -75,6 +81,17 @@ export PATH=$GW_LOCATION/bin:$PATH
 
 # GridWay DRMAA Python (also needs LD_LIBRARY_PATH to include 
 export PYTHONPATH=$GW_LOCATION/lib/python:${PYTHONPATH}
+
+# PostgreSQL
+export PGSQL_LOCATION=$INSTMACH/pgsql
+export PATH=$PGSQL_LOCATION/bin:$PATH
+export PGSQL_DATA=$PGSQL_LOCATION/data
+export PGDATA=$PGSQL_DATA # PostgreSQL recognizes this
+export LD_LIBRARY_PATH="${PGSQL_LOCATION}/lib:${LD_LIBRARY_PATH}"
+## PostgreSQL client psql will try to get passwords from this file -
+## this is th way to avoid password prompts for maintenance scripts
+export PGPASSFILE=$PRIV_DATA/pgpass
+export PGSQL_ROOT_USER=postgres
 
 # Ruby stuff
 # We need to have rubygems module loaded ("require") on every start of ruby,
