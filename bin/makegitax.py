1d0fc814a1229a3f1c9cc33fce69e5e761787774
Andrey Tovchigrechko <atovtchi@jcvi.org>
2013-04-23 13:52:43 -0400

    Better option handling for taxonomic DB pre-processing

--- bin/makegitax.py
+++ bin/makegitax.py
@@ -13,12 +13,11 @@ from MGT.Taxa import *
 def getProgOptions():
     from optparse import OptionParser, make_option
     option_list = [
-        make_option("-i", "--in-dump",
-        action="append", type="string",dest="inDump"),
         make_option("-o", "--out-bin-file",
         action="store", type="string",dest="outFile"),
     ]
-    parser = OptionParser(usage = "usage: %prog [options]",option_list=option_list)
+    parser = OptionParser(usage = \
+            "usage: %prog [options] gi-to-taxa.dmp [,gi-to-taxa.dmp...]",option_list=option_list)
     (options, args) = parser.parse_args()
 
     return options,args
@@ -26,6 +25,5 @@ def getProgOptions():
 
 opt,args = getProgOptions()
 
-
-makeGiTaxBin(opt.inDump,opt.outFile)
+makeGiTaxBin(args,opt.outFile)
 
