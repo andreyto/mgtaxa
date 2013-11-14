daf4cd8d7b07357b2400d876f9d9ff7a6935827d
Andrey Tovchigrechko <atovtchi@jcvi.org>
2013-11-14 01:19:29 -0500

    Sample script runs makeflow

--- bin/onetime/build-ref-db/run_make_seqdb.sh
+++ bin/onetime/build-ref-db/run_make_seqdb.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -ex
+#python $MGT_HOME/MGT/ImmClassifierApp.py --mode make-ref-seqdb \
 mgt-icm-classifier --mode make-ref-seqdb \
---inp-ncbi-seq $MGT_NCBI_DB'/refseq/*.fna.gz' \
---db-seq $MGT_DATA/refseq-seqdb --run-mode inproc \
---lrm-user-options '-P 0413'
+--inp-ncbi-seq $MGT_NCBI_DB'/refseq/*/*.fna.gz' \
+--db-seq $MGT_DATA/refseq-seqdb \
+--run-mode batchDep \
+--lrm-user-options '-P 9223' \
+--batch-backend makeflow \
+--workflow-file make-ref-seqdb.mkf
+
+MAKEFLOW_BATCH_QUEUE_TYPE=sge BATCH_OPTIONS='-P 9223 -b n -l fast' $MGT_HOME/bin/mgt_wrapper makeflow -J 1000 make-ref-seqdb.mkf
 
