afb6f6f744bde9cca2d11e27a3557deb92b4bf8e
Andrey Tovchigrechko <atovtchi@jcvi.org>
2013-11-15 11:17:26 -0500

    Make seqdb and train models worked smooth (13K models built in 6K CPU*hr)

--- bin/onetime/build-ref-db/run_make_seqdb.sh
+++ bin/onetime/build-ref-db/run_make_seqdb.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -ex
-#python $MGT_HOME/MGT/ImmClassifierApp.py --mode make-ref-seqdb \
-mgt-icm-classifier --mode make-ref-seqdb \
+if true; then
+#mgt-icm-classifier --mode make-ref-seqdb \
+python $MGT_HOME/MGT/ImmClassifierApp.py --mode make-ref-seqdb \
 --inp-ncbi-seq $MGT_NCBI_DB'/refseq/*/*.fna.gz' \
 --db-seq $MGT_DATA/refseq-seqdb \
 --run-mode batchDep \
 --lrm-user-options '-P 9223' \
 --batch-backend makeflow \
 --workflow-file make-ref-seqdb.mkf
+fi
 
-MAKEFLOW_BATCH_QUEUE_TYPE=sge BATCH_OPTIONS='-P 9223 -b n -l fast' $MGT_HOME/bin/mgt_wrapper makeflow -J 1000 make-ref-seqdb.mkf
+MAKEFLOW_BATCH_QUEUE_TYPE=sge \
+    BATCH_OPTIONS='-P 9223 -b n -l fast' \
+    $MGT_HOME/bin/mgt_wrapper \
+    makeflow -J 1000 -r 3 -S 30 \
+    make-ref-seqdb.mkf
 
--- bin/onetime/build-ref-db/run_train_icm.sh
+++ bin/onetime/build-ref-db/run_train_icm.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-cp /usr/local/projects/GOSII/atovtchi/ref.extra.seqdb/*.fasta* $MGT_DATA/refseq-seqdb/
+set -ex
+if true; then
+#cp /usr/local/projects/GOSII/atovtchi/ref.extra.seqdb/*.fasta* $MGT_DATA/refseq-seqdb/
 mgt-icm-classifier --mode train \
 --db-imm $MGT_DATA/icm-refseq \
 --db-seq $MGT_DATA/refseq-seqdb \
---incremental-work 1 \
 --run-mode batchDep \
---lrm-user-options '-P 0413'
+--batch-backend makeflow \
+--workflow-file train-icm-ref.mkf
+fi
+
+MAKEFLOW_BATCH_QUEUE_TYPE=sge \
+    BATCH_OPTIONS='-P 9223 -b n -l fast' \
+    $MGT_HOME/bin/mgt_wrapper \
+    makeflow -J 1000 -r 3 -S 30 \
+    train-icm-ref.mkf
+
 
