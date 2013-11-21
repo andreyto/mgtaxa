19e85973afc513852904eef44d929d212ebc49eb
Andrey Tovchigrechko <atovtchi@jcvi.org>
2013-11-21 13:34:26 -0500

    In addition to taxonomy, the identity of the winning model is saved

--- MGT/TaxaPred.py
+++ MGT/TaxaPred.py
@@ -15,19 +15,25 @@ class TaxaPred(object):
         else:
             self.hdfFile = hdfFile
 
-    def initForAppend(self,idSampDtype=None,expectedrows=None):
+    def initForAppend(self,idSampDtype=None,idScoreDtype=None,expectedrows=None):
         if idSampDtype is None:
             idSampDtype = idDtype
+        if idScoreDtype is None:
+            idScoreDtype = idDtype
         f = self.hdfFile
         g = f.createGroup(f.root,self.group)
         g._v_attrs.kind = "TaxaPred"
         f.createEArray(g, 'idSamp', pt.Atom.from_dtype(n.dtype(idSampDtype)),(0,),"Sample ID",expectedrows=expectedrows)
+        f.createEArray(g, 'predIdScore', pt.Atom.from_dtype(n.dtype(idScoreDtype)),(0,),
+                "Predicted Score ID",expectedrows=expectedrows)
         f.createEArray(g, 'lenSamp', pt.Int64Atom(),(0,),"Sample ID",expectedrows=expectedrows)
         f.createEArray(g, 'predTaxid',pt.Int32Atom(),(0,),"Predicted Taxid",expectedrows=expectedrows)
         f.createEArray(g, 'predScore',pt.Float32Atom(),(0,),"Score of prediction",expectedrows=expectedrows)
         f.flush()
     
-    def initFromSamp(self,idSamp,lenSamp):
+    def initFromSamp(self,idSamp,lenSamp,idScoreDtype=None):
+        if idScoreDtype is None:
+            idScoreDtype = idDtype
         assert len(idSamp) == len(lenSamp)
         f = self.hdfFile
         g = f.createGroup(f.root,self.group)
@@ -40,6 +46,8 @@ class TaxaPred(object):
         #from numpy array or python sequence, but not from another tables.Array
         #f.createArray(g, 'idSamp', idSamp,"Sample ID")
         idSamp._f_copy(g)
+        f.createArray(g, 'predIdScore',n.zeros(len(idSamp),dtype=n.dtype(idScoreDtype)), 
+                "Predicted Score ID")
         lenSamp._f_copy(g)
         f.createArray(g, 'predTaxid',n.zeros(len(idSamp),dtype="i4"),"Predicted Taxid")
         f.createArray(g, 'predScore',n.zeros(len(idSamp),dtype="f4"),"Score of prediction")
