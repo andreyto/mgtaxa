379014f2c4fe97d0660a1baf0578d34a0149550d
Andrey Tovchigrechko <atovtchi@jcvi.org>
2013-12-10 15:08:59 -0500

    Steps in manually edited makeflows use env wrapper. Benchmark completed on NC scaffolds against scaffolds

---
+++ bin/onetime/gos/bin-cn/run_bench_cn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+
+export BATCH_OPTIONS='-P 9223 -b n -l medium'
+export MAKEFLOW_BATCH_QUEUE_TYPE=sge
+export MAKEFLOW_MAX_REMOTE_JOBS=500
+
+this_dir=$(dirname $0)
+
+$MGT_HOME/bin/mgt_wrapper \
+    makeflow -r 3 -S 30 \
+    $this_dir/bench_cn.mkf
+
