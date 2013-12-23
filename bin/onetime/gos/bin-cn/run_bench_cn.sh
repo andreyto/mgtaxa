b6c65c2bc8fcfc32b687c700fbb9c2dcf10441d8
Andrey Tovchigrechko <atovtchi@jcvi.org>
2013-12-23 13:32:14 -0500

    Updated high-level workflows

--- bin/onetime/gos/bin-cn/run_bench_cn.sh
+++ bin/onetime/gos/bin-cn/run_bench_cn.sh
@@ -2,12 +2,12 @@
 set -ex
 
 export BATCH_OPTIONS='-P 9223 -b n -l medium'
-export MAKEFLOW_BATCH_QUEUE_TYPE=local
+export MAKEFLOW_BATCH_QUEUE_TYPE=sge
 export MAKEFLOW_MAX_REMOTE_JOBS=500
 
 this_dir=$(dirname $0)
 
 $MGT_HOME/bin/mgt_wrapper \
-    makeflow -r 3 -S 30 \
+    makeflow -r 3 -S 30 -J 500 \
     $this_dir/bench_cn.mkf
 
